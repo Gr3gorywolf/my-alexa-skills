586460c225c436f053863e05096a07fe6d82fb77
Gregory Cabral De Los Santos <Blackarcanjus@gmail.com>
2020-12-27 11:40:07 -0400

    First commit :fire:

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
+const Speech = require("ssml-builder");
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+function getResObject(outputSpeech) {
+    return {
+        "version": "1.0",
+        "sessionAttributes": {
+
+        },
+        "response": {
+            "outputSpeech": {
+                "type": "SSML",
+                "ssml": `<speak>${outputSpeech}</speak>`,
+            },
+            "shouldEndSession": false
+        }
+    }
+}
+
+app.post("/crackwatch", (req, res) => {
+    console.log(req.body);
+    var speech = new Speech();
+    fetch("https://api.crackwatch.com/api/games?page=0&is_cracked=true")
+        .then(response => response.json())
+        .then(body => {
+            console.log(body);
+            speech.say("los ultimos juegos crackeados son los siguientes:")
+                .pause("300ms");
+            for (let game of body.slice(0, 3)) {
+                speech.say(game.title).pause("300ms");
+            }
+        }).catch((err) => {
+            speech.say("Error al obtener los ultimos juegos crackeados");
+        }).finally(() => {
+            res.json(getResObject(speech.ssml(true)));
+        })
+});
+
+app.listen(9000, () => {
+    console.log("listening on port 9000");
+});
